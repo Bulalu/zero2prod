552a40a3bf023f6acb0dd82ebc7ce5001b41eb12
Bulalu <elishabulalu031@gmail.com>
2023-09-05 01:02:29 +0300

    added Pg connection to the app

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,29 @@
 use crate::routes::{health_check, subscribe};
-
-use actix_web::{web, App, HttpServer};
-use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer, dev::Server};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub  fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| { App::new()
-        .route("/health_check", web::get().to(health_check))
-        .route("/subscriptions", web::post().to(subscribe))
+// Function to start and run the HTTP server.
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the database connection in Actix web::Data for sharing across handlers.
+    let connection = web::Data::new(connection);
+
+    // Start building the Actix web server.
+    let server = HttpServer::new(move || {
+        // Create a new Actix App.
+        App::new()
+            // Attach the `health_check` handler to `/health_check` route.
+            .route("/health_check", web::get().to(health_check))
+            // Attach the `subscribe` handler to `/subscriptions` route.
+            .route("/subscriptions", web::post().to(subscribe))
+            // Attach the cloned database connection to the App.
+            .app_data(connection.clone())
     })
+        // Bind the server to the provided TcpListener.
         .listen(listener)?
+        // Start the server and begin listening for requests.
         .run();
 
+    // Return the Actix Server instance.
     Ok(server)
-
 }
