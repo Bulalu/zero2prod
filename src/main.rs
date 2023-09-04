fce15c2540ea64bcf58a61541ea612437bb8a7d7
Bulalu <elishabulalu031@gmail.com>
2023-09-05 00:39:57 +0300

    connected db with tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")?; // Bind to a random port
-
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
 
