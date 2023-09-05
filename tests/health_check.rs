ff53c86b545e84a3d20875c1a3ae0d919bb5b624
Bulalu <elishabulalu031@gmail.com>
2023-09-05 13:23:19 +0300

    passing tests 🦀

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -97,17 +97,16 @@ async fn spawn_app() -> TestApp {
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
     let server = run(listener, connection_pool.clone())
         .expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
+
+
+
 
     TestApp {
         address,
