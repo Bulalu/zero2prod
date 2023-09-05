2f54ec3300568ddbd929d67742611bea35b1f579
Bulalu <elishabulalu031@gmail.com>
2023-09-05 17:37:52 +0300

    added logs 🩻

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -19,7 +19,17 @@ pub async fn subscribe(
     pool: web::Data<PgPool>
 
 ) -> HttpResponse {
-    log::info!("Saving new subscriber details in the database");
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        _form.email,
+        _form.name
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -32,13 +42,20 @@ pub async fn subscribe(
     ).execute(pool.as_ref())
         .await {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!(
+                "request_id {} - New subscriber details have been saved", request_id
+            );
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
 
 }
+
